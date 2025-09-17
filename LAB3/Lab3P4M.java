bc63aaeb286b550b1e15ec56987f9071f05ad86c
shivasai8545-byte <shivasai8545@gmail.com>
2025-09-17 12:56:18 +0530

    Add files via upload

---
+++ LAB3/Lab3P4M.java
@@ -0,0 +1,9 @@
+class Lab3P4M{
+	public static void main(String...args){
+		Lab3P42 p = new Lab3P42();
+		
+		p.setName("ACE ENGINEERING COLLEGE");
+		System.out.println(p.getName());
+		p.demonstrate();
+	}
+}
